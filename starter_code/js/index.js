5ef3f57ae4160eb6eccdc011ab12095d26ebd029
nicaela <nici.dwight@gmail.com>
2016-12-20 18:38:32 +1100

    Finally got it working - thanks grace!

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -1,15 +1,19 @@
+$(document).ready(start);
+
 function start() {
 	$(".readmore").click(expandText);
 
-	function expandText () {
+	function expandText (event) {
+		event.preventDefault();
 	       $('#show-this-on-click').slideDown();
-	       $('.readless hide').show();
+	       $('.readless').show();
 	       $('.readmore').hide();
 	   }
 	   
 	 $(".readless").click(minimiseText);
 
-	 function minimiseText () {
+	 function minimiseText (event) {
+	 	event.preventDefault();
 	       $('#show-this-on-click').slideUp();
 	       $('.readmore').show(300);
 	       $('.readless').hide();
@@ -17,10 +21,10 @@ function start() {
 	   
 	$(".learnmore").click(expandSideBar);
 
-	function expandSideBar() {
+	function expandSideBar(event) {
+		event.preventDefault();
 	       $('#learnmoretext').slideDown();
 	       $('.learnmore').hide();
 	   }
 }
 
-$(document).ready(start);
